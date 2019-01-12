6b865a328921947e43864772a727dc0ace153127
troynguyen8@gmail.com <troynguyen8@gmail.com>
2019-01-12 14:20:13 -0600

    small refactor, added functional nav with smooth scrolling via jQuery animate

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
-$(function () {
-  // TODO: Add any necessary handlers
-});
+(function () {
+  /**
+   * Assigns Nav bar links to smooth scroll to section based on hash and section id
+   */
+  function assignNavLinkClickHandlers() {
+    $('.navbar-nav .nav-link').on('click', function (e) {
+      e.preventDefault(); // prevent jumping straight to section
+
+      const target = this.hash;
+      const $target = $(target); // use hash as a selector to find the section by id
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 700, 'swing', function () {
+        window.location.hash = target;
+      });
+    });
+  }
+
+  $(function () {
+    // Add handlers after document is ready
+    assignNavLinkClickHandlers();
+  });
+})();
