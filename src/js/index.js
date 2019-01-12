2dbaab832d10cd9443500aefce2e735e0e2db03c
troynguyen8@gmail.com <troynguyen8@gmail.com>
2019-01-12 13:51:40 -0600

    added partners and corporate sponsors. holding off on mentors for now due to lack of logo. Changed padding and spacing for logos. Changed expand / contract logic to CSS instead of JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,3 @@
 $(function () {
-  const $sponsorImages = $('.sponsor-img');
-
-  // grow / shrink sponsor images on mouseenter / mouseleave
-  $sponsorImages
-    .on('mouseenter', function () {
-      $(this).animate({
-        margin: -10,
-        width: '+=20',
-        height: '+=20'
-      });
-    })
-    .on('mouseleave', function () {
-      $(this).animate({
-        margin: 0,
-        width: '-=20',
-        height: '-=20'
-      })
-    });
+  // TODO: Add any necessary handlers
 });
