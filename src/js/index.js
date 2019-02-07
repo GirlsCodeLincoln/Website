0f0b8b5d8f5ce733ceafdbd1a137355f139f2de5
troynguyen8@gmail.com <troynguyen8@gmail.com>
2019-02-06 19:03:11 -0600

    added social links, moved email to an obfuscated mailto: link

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
    * Assigns Nav bar links to smooth scroll to section based on hash and section id
    */
   function assignNavLinkClickHandlers() {
-    $('.navbar-nav .nav-link').on('click', function (e) {
+    $('.nav').on('click', function (e) {
       e.preventDefault(); // prevent jumping straight to section
 
       const target = this.hash;
@@ -27,9 +27,49 @@
     });
   }
 
+  /**
+   * Obfuscate email and assign href to email icon
+   */
+  function injectMailToButton() {
+    const getEmailButtonHtmlStr = function (link) {
+      return '<a href="mailto:' + link + '" aria-label="Email"><i class="fas fa-envelope fa-2x mx-2" aria-hidden="true"></i></a>';
+    }
+
+    // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+    // Random encryption key feature coded by Andrew Moulden
+    // This code is freeware provided these four comment lines remain intact
+    // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+    coded = "Js0N@iJ96ZyNFv6JsyN6s.yNo"
+    key = "oaIMb8y7e3dG4WRQEZYB0qlHiNkVSnrPtvjFCmhDwLKcp1A6UJ5O2zs9TfuXxg"
+    shift = coded.length
+    link = ""
+    for (i = 0; i < coded.length; i++) {
+      if (key.indexOf(coded.charAt(i)) == -1) {
+        ltr = coded.charAt(i)
+        link += (ltr)
+      }
+      else {
+        ltr = (key.indexOf(coded.charAt(i)) - shift + key.length) % key.length
+        link += (key.charAt(ltr))
+      }
+    }
+
+    $('#social-links').prepend(getEmailButtonHtmlStr(link));
+  }
+
+  /**
+   * Programatically add copyright message with current year
+   */
+  function injectCopyrightStatement() {
+    const CURR_YEAR = (new Date()).getFullYear();
+    $('#copyright-container').text('Copyright © ' + CURR_YEAR + ' Girls Code Lincoln');
+  }
+
   $(function () {
     // Add handlers after document is ready
     assignNavLinkClickHandlers();
     assignTimelineShowMoreHandler();
+    injectMailToButton();
+    injectCopyrightStatement();
   });
 })();
