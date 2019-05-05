249181e039a4a4aed266d76ac8d424343a2daace
Troy Nguyen <troynguyen8@gmail.com>
2019-05-04 22:58:31 -0500

    Added Image Gallery and Background Images for Sections (#78)
    * added sponsor and clubs background with heavy translucent color filter
    
    * continued experiementing with redesign
    
    * added image gallery using bootstrap carousel cross fade

--- src/js/index.js
+++ src/js/index.js
@@ -22,6 +22,70 @@
   }
 
   /**
+   * Adds all images to the image gallery carousel programatically to reduce clutter in the HTML
+   */
+  function appendImageGalleryImages() {
+    // constructs markup for HTML structure:
+    // <div class="carousel-item><img class="d-block" src="someSrc" alt="someAlt"></div>
+    // isActive is a boolean. When true, the class "active" is added to the carousel-item outer div
+    const getCarouselImageItemMarkup = function(src, alt, isActive) {
+      let carouselItemClass = '"carousel-item';
+      carouselItemClass += (isActive) ? ' active"' : '"';
+
+      return '<div class=' + carouselItemClass +'><img class="d-block" src="' + src + '" alt="' + alt + '"></div>';
+    };
+
+    // constructs markup for HTML structure:
+    // <li data-target="#image-carousel" data-slide-to="someIndex"></li>
+    // isActive is a boolean. When true, the class "active" is added to the li
+    const getCarouselIndicatorsItemMarkup = function(index, isActive) {
+      const carouselIndicatorClass = (isActive) ? ' class="active"' : '';
+      return '<li data-target="#image-carousel" data-slide-to="' + index + '"' + carouselIndicatorClass + '></li>'; 
+    }
+
+    // array of objects denoting src of img, alt text, and any custom css applied to the image
+    const images = [
+      {
+        src: '/assets/img/gallery/girlscode-17.jpg',
+        alt: 'Girl Smiling While Coding',
+      },
+      {
+        src: '/assets/img/gallery/girlscode-1.jpg',
+        alt: 'Girls With Yarn During a Leadership Activity',
+      },
+      {
+        src: '/assets/img/gallery/girlscode-4.jpg',
+        alt: 'Girls High Fiving',
+        css: { 'object-position': '25% center' }
+      },
+      {
+        src: '/assets/img/gallery/girlscode-18.jpg',
+        alt: 'Girl Focusing While Coding',
+      },
+      {
+        src: '/assets/img/gallery/girlscode-19.jpg',
+        alt: 'Volunteer and Two Girls Laughing While Coding',
+      }
+    ];
+
+    const $galleryInnerCarousel = $('#image-gallery .carousel-inner');
+    const $galleryCarouselIndicators = $('#image-gallery .carousel-indicators');
+
+    let isFirstImage = true;
+    images.forEach(function (imageData, index) {
+      $galleryInnerCarousel.append(getCarouselImageItemMarkup(imageData.src, imageData.alt, isFirstImage));
+
+      if (imageData.css) {
+        const $image = $($galleryInnerCarousel.children().children()[index]);
+        $image.css(imageData.css);
+      }
+
+      $galleryCarouselIndicators.append(getCarouselIndicatorsItemMarkup(index, isFirstImage));
+      isFirstImage = false;
+    });
+  }
+
+  /**
    * Shows and hides previous clubs when clicked
    */
   function assignTimelineShowMoreHandler() {
@@ -43,6 +107,7 @@
   $(function () {
     // Add handlers after document is ready
     assignNavLinkClickHandlers();
+    appendImageGalleryImages();
     assignTimelineShowMoreHandler();
     injectCopyrightStatement();
   });
