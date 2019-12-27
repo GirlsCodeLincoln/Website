5ffb9175947363e3d0cc9e3e421311b2a36c4f87
Troy Nguyen <tnguyen70@unl.edu>
2019-12-27 17:00:30 -0600

    Attempt to fix failing selenium test

--- src/tests/test_gcl_page.py
+++ src/tests/test_gcl_page.py
@@ -49,7 +49,7 @@ def test_join_club(browser):
 	"""
 	page = GCLPOM(browser)
 	page.load()
-	page.test_join_workshop()
+	page.test_join_club()
 	url = 'http://localhost:8080'
 	assert browser.current_url == url
 	browser.quit()
