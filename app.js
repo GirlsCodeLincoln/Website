e807c3f1c9db215546cf53ad1b53345080b1b166
Justin J Stark <fromgithub@justinjstark.com>
2021-01-06 21:36:35 -0600

    Remove / express path

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const port = 3000
 
 app.use(express.static('src'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 })
